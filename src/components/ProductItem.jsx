96ac5fe8124254fe660ac407b0dd32ed935501e2
Omprakash Kumar Singh <kamalrajput99550@gmail.com>
2025-12-25 04:52:01 +0530

    Initial commit for E-commerce Website

---
+++ src/components/ProductItem.jsx
@@ -0,0 +1,23 @@
+import React, { useContext } from "react";
+import { ShopContext } from "../context/Shopcontext";
+import { Link } from "react-router-dom";
+
+const ProductItem = ({ id, image, name, price }) => {
+  const { currency } = useContext(ShopContext);
+
+  return (
+    <Link className="text-gray-700 cursor-pointer" to={`/Product/${id}`}>
+      <div className="overflow-hidden">
+        <img
+          className="hover:scale-110 transition ease-in-out"
+          src={Array.isArray(image) ? image[0] : image}  
+          alt={name} 
+        />
+      </div>
+      <p className="pt-3 pb-1 text-sm">{name}</p>
+      <p className="text-sm font-medium">{currency}{price}</p>
+    </Link>
+  );
+};
+
+export default ProductItem;
