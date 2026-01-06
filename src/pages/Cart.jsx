ecf5c613ce819716a530823c853a9bc1466bbd2d
Omprakash Kumar Singh <kamalrajput99550@gmail.com>
2026-01-06 23:56:19 +0530

    update

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/Shopcontext";
+import Title from "../components/Title";
 
-function Cart() {
+const Cart = () => {
+  const { products, currency, cartItems } = useContext(ShopContext);
+  const [cartData, setCartData] = useState([]);
+  useEffect(() => {
+    const tempData = [];
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        if (cartItems[items][item] > 0) {
+          tempData.push({
+            _id: items,
+            size: item,
+            quantity: cartItems[items][item],
+          });
+        }
+      }
+    }
+    console.log(tempData);
+  }, [cartItems]);
   return (
-    <div>
-      
+    <div className="border-t pt-14">
+      <div className="text-2xl mb-3">
+        <Title text1={"YOUR"} text2={"CART"} />
+      </div>
+      <div>
+        {cartData.map((item, index) => {
+          const productData = productData.find(
+            (products) => products._id === item._id
+          );
+          return (
+            <div
+              key={index}
+              className="py-4 border-t border-b text-gray-700  grid grid-cols-[4fr_0.5fr_0.5fr sm:grid-cols-[4fr_2fr_.5fr] items-center gap-4"
+            ></div>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
