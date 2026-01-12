f12bcb654379f4789d91e0fa64e65bbbb88c1678
Omprakash Kumar Singh <kamalrajput99550@gmail.com>
2026-01-12 22:30:17 +0530

    Back-end 2nd

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
-import  express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+// App config
+const app = express();
+const port = process.env.PORT || 4000;
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
