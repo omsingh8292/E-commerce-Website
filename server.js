e93ed4c0818f2c68e6465e319f398ccbd8d2a737
Omprakash Kumar Singh <kamalrajput99550@gmail.com>
2026-01-13 22:10:20 +0530

    Add Back-End

--- server.js
+++ server.js
@@ -9,3 +9,11 @@ const port = process.env.PORT || 4000;
 // Middlewares
 app.use(express.json());
 app.use(cors());
+
+//API endpoints
+
+app.get("/", (req, res) => {
+  res.send("API IS WORKING");
+});
+
+app.listen(port,()=>console.log('Server started on PORT:'+port));
