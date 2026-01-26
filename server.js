21c746bc191728f750d167ce3467c6976e11137b
Omprakash Kumar Singh <kamalrajput99550@gmail.com>
2026-01-26 21:31:56 +0530

    makes changes

--- server.js
+++ server.js
@@ -18,16 +18,3 @@ app.get("/", (req, res) => {
 
 app.listen(port,()=>console.log('Server started on PORT:'+port));
 
-// the thing is to define the all the tet
-
-
-
-// deploy  all the wrok and allow all theri context in he file and ac
-// 
-
-// im not able to solve my question which i solved inmy past
-//knowledge heri perfection an allow all the task and their data is to allow the tassk for its future
-// the thing is good and look all the task aer work perfctly and do all thejob
-//hey there is good guy
-// the thing is good and all in same way
-// the thing is all the thing are sorted the data are  driven through the all the syntaxand covered by all the task and genrated by the tither abd describe all the things in vommon way and talk politely and must he all  
