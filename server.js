0ce91b99cda5621faccdf9d5598cfa75c2176c61
Omprakash Kumar Singh <kamalrajput99550@gmail.com>
2026-01-28 23:25:52 +0530

    installing mango

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectDB from "./config/Mongodb.js";
+import connectcloudinary from "./config/Cloudinary.js";
 
 // App config
 const app = express();
 const port = process.env.PORT || 4000;
+connectDB(),
+connectcloudinary()
 
 // Middlewares
 app.use(express.json());
