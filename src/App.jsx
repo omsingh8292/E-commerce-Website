55ac2477ada5d8abf33cb4a2a38df96c29949aff
Omprakash Kumar Singh <kamalrajput99550@gmail.com>
2026-02-17 18:41:05 +0530

    pop

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useEffect } from "react";
 
+// the tree is clean
+
 export const backendUrl = import.meta.env.VITE_BACKEND_URL;
 export const currency = "$"
 
